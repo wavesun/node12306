90e94eadc87b11778c96f14d4df1ba0eb4d160e0
dotSlash at work <lornereg@126.com>
2013-01-23 13:04:51 +0800

    now it handles cookies

--- routes/captcha.js
+++ routes/captcha.js
@@ -3,24 +3,54 @@ var http = require('http'),
     config = require('../config.js'),
     log = require(config.helpers + 'log.js')(__filename);
 
-var options = {
-  host: config.login.captchaHost, 
-  path: config.login.captchaPath, 
-  headers: {"user-agent": config.login.ua}
-};
-
 module.exports = function(request, respond)
 {
+  var options = {
+    host: config.login.captchaHost, 
+    path: config.login.captchaPath, 
+    headers: {"user-agent": config.login.ua}
+  };
+
   var req = http.request(options, function(res){
     log('Getting captcha...');
-    var f = fs.createWriteStream(config.login.captchaSavePath + '/captcha.jpg', 
-                             {flags: "a", encoding: 'binary'});
-                             
+    var f = fs.createWriteStream(
+        config.login.captchaSavePath + '/captcha.jpg', 
+        {flags: "w", encoding: 'binary'}
+    );
+
     res.on('data', function(data){
       f.write(data);
     })
   
     res.on('end', function(){
+      //handle cookies
+      var setCookie = res.headers['set-cookie'], cookies = [], cookieString = "";
+      if(setCookie.length)
+        setCookie.forEach(function(v, i){
+          var cols, key, value, path;
+          cols = v.split(';');
+          if(cols.length == 2)
+          {
+            key = cols[0].split('=')[0];
+            value = cols[0].split("=")[1];
+            path = cols[1].split("=")[1];
+          }
+          cookies.push({
+            key: key,
+            value: value,
+            path: path,
+          });
+        })
+      cookies.forEach(function(v, i){
+        cookieString += 'dynamic.12306.cn\tFALSE\t' + v.path + '\tFALSE\t0\t' + v.key + '\t' + v.value + '\r';
+      })
+      fs.writeFile(config.cookiePath + '/cookie.txt', cookieString, function(err){
+        if(err)
+          log('saving cookies error: ' + err);
+        else
+          log('successfully saved cookies');
+      });
+
       f.end();
       log('Got captcha');
       respond.json(1);
